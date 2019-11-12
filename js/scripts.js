e678ddf521f2272c8c94be2e07330ad8791252ef
Janelle <janellezucc@gmail.com>
2019-11-11 20:23:10 -0500

    adding span to image tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-const images = $('.images a').lightbox();
-
-let searchBox = document.querySelector('#search');
-let pictures = document.querySelectorAll('a');
-searchBox.addEventListener('keyup', () => {
-    for(let i = 0; i < pictures.length; i++) {
-        if(pictures[i].dataset.title.toLocaleLowerCase().includes(searchBox.value.toLowerCase())) {
-            pictures[i].style.display = 'inline';
-        } else {
-            pictures[i].style.display = 'none';
-        } 
-    } 
-});
-
 $(document).ready(function() {
-	$('#search').hideseek();
+    $('#search').hideseek();
 });
